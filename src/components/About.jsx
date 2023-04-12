46a55fca5f43d6d28b9139bd76a7fa51440fa25e
Sebiten <sebdevspace@gmail.com>
2023-04-12 13:39:20 -0300

    about added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,54 +1,54 @@
-import Image from "next/image";
 import React from "react";
+import Image from "next/image";
 
-const About = () => {
+const AboutUsSection = () => {
   return (
-    <div className="bg-white text-gray-800 py-20">
-      <div className="max-w-5xl mx-auto px-6">
-        <div className="flex items-center mb-10">
-          <Image
-            height={80}
-            width={80}
-            className="w-16 h-16 mr-4"
-            src="https://via.placeholder.com/80x80"
-            alt="Logo Boost Digital Solutions"
-          />
-          <h2 className="text-3xl font-semibold">Sobre Nosotros</h2>
-        </div>
-        <div className="flex flex-col md:flex-row">
-          <div className="md:w-1/2">
-            <p className="text-lg mb-4">
-              En Boost Digital Solutions, nos apasiona ayudar a las empresas a
-              alcanzar su máximo potencial en línea. Con más de 10 años de
-              experiencia en el campo, nuestro equipo de expertos se ha dedicado
-              a ofrecer soluciones personalizadas y de alta calidad en el
-              desarrollo web, diseño de imagen empresarial, marketing digital y
-              mucho más.
-            </p>
-            <p className="text-lg mb-4">
-              Sabemos que cada empresa es única, por lo que trabajamos
-              estrechamente con nuestros clientes para comprender sus
-              necesidades específicas y ofrecer soluciones que se adapten a sus
-              objetivos y presupuestos. Nos enorgullece nuestro compromiso con
-              la satisfacción del cliente y siempre nos esforzamos por superar
-              las expectativas.
-            </p>
-            <p className="text-lg">
-              ¡Permítenos ayudarte a llevar tu empresa al siguiente nivel en
-              línea!
-            </p>
+    <section className="bg-gray-100 py-16">
+      <div className="container mx-auto">
+        <div className="flex flex-wrap items-center">
+          <div
+            className="
+              max-w-6xl mx-auto
+              "
+          >
+            <div className="relative h-80 w-full overflow-hidden rounded-lg">
+              <Image
+                src="/logo.png"
+                width={100}
+                height={50}
+                alt="About Us"
+                className="transition duration-500 ease-in-out transform hover:scale-105 w-full"
+              />
+            </div>
           </div>
-          <div className="md:w-1/2 flex justify-center items-center">
-            <img
-              src="https://via.placeholder.com/500x500"
-              alt="Equipo de Boost Digital Solutions"
-            />
+          <div className="w-full md:w-1/2 lg:w-2/3 pl-0 md:pl-16">
+            <h2 className="text-3xl md:text-4xl font-bold mb-4 text-gray-800">
+              About Us
+            </h2>
+            <p className="text-gray-600 mb-6 leading-relaxed">
+              Welcome to our website! We are a team of passionate individuals
+              who are dedicated to creating innovative and cutting-edge
+              solutions for our clients. With over 10 years of experience in the
+              industry, we have built a reputation for delivering high-quality
+              results that exceed our clients expectations.
+            </p>
+            <p className="text-gray-600 mb-6 leading-relaxed">
+              Our mission is to help businesses grow and thrive in the digital
+              world. We offer a wide range of services, including web design,
+              web development, digital marketing, and branding. Our team of
+              experts uses the latest technologies and industry best practices
+              to create unique and impactful solutions that drive results.
+            </p>
+
+            <p className="text-gray-600 leading-relaxed">
+              Contact us today to learn more about how we can help your business
+              succeed online!
+            </p>
           </div>
         </div>
       </div>
-      <div className="bg-gray-900 opacity-75 absolute top-0 left-0 w-full h-full z-0"></div>
-    </div>
+    </section>
   );
 };
 
-export default About;
+export default AboutUsSection;
--- src/pages/index.js
+++ src/pages/index.js
@@ -27,6 +27,7 @@ export default function Home() {
         <>
           <Navbar />
           <Hero />
+          <About/>
           <PlansSection/>
           <Plan/>
           <Reviews/>
