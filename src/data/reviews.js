058542e43a9905246b4213ea8053f86ae2f63cff
Sebiten <sebdevspace@gmail.com>
2023-05-15 18:56:07 -0300

    ss

--- src/data/reviews.js
+++ src/data/reviews.js
@@ -1,37 +1,35 @@
-
-
 export const reviews = [
   {
     id: 1,
-    name: "Juan Perez",
-    date: "Marzo 10, 2023",
+    name: "Juan Pérez",
+    date: "10 de marzo de 2023",
     rating: 4,
     review:
-      "Excelente comida y atención. Recomiendo especialmente el plato de pollo al curry. Volvería sin dudarlo.",
+      "Contraté esta startup para diseñar mi sitio web y hicieron un trabajo fantástico. El diseño es limpio y profesional, y la funcionalidad es de primera calidad. Estoy muy satisfecho con su trabajo.",
     male: true,
   },
   {
     id: 2,
-    name: "Ana Rodriguez",
-    date: "Marzo 8, 2023",
+    name: "Ana Rodríguez",
+    date: "8 de marzo de 2023",
     rating: 5,
     review:
-      "Uno de los mejores restaurantes que he visitado. La comida es deliciosa y el ambiente es muy acogedor. Definitivamente volveré pronto.",
+      "Estoy extremadamente feliz con el diseño del logotipo que recibí de esta startup. Captura perfectamente la esencia de mi marca y el equipo fue un placer trabajar con ellos. ¡Altamente recomendado!",
     female: true,
   },
   {
     id: 3,
-    name: "Pedro Sanchez",
-    date: "Marzo 7, 2023",
+    name: "Pedro Sánchez",
+    date: "7 de marzo de 2023",
     rating: 3,
     review:
-      "El servicio fue excelente, pero la comida no fue tan buena como esperaba. No volvería.",
+      "El diseño del sitio web que obtuve de esta startup fue decente, pero no cumplió completamente mis expectativas. Hubo algunos problemas menores de capacidad de respuesta, pero en general, sigue siendo un producto sólido.",
     male: true,
   },
   {
     id: 4,
-    name: "Maria Lopez",
-    date: "Marzo 6, 2023",
+    name: "María López",
+    date: "6 de marzo de 2023",
     rating: 4,
     review:
       "El ambiente es muy agradable y el servicio fue muy bueno. La comida estaba deliciosa, pero no fue lo suficientemente buena como para volver.",
@@ -39,21 +37,20 @@ export const reviews = [
   },
   {
     id: 5,
-    name: "Jose Garcia",
-    date: "Marzo 5, 2023",
+    name: "José García",
+    date: "5 de marzo de 2023",
     rating: 5,
     review:
-      "El mejor restaurante que he visitado. La comida era deliciosa y el servicio fue excelente. Sin duda volveré pronto.",
+      "La mejor startup que he visitado. El diseño del logotipo era delicioso y el servicio fue excelente. Sin duda volveré pronto.",
     male: true,
   },
-
   {
     id: 6,
-    name: "Luisa Martinez",
-    date: "Marzo 4, 2023",
+    name: "Luisa Martínez",
+    date: "4 de marzo de 2023",
     rating: 5,
     review:
-      "El mejor restaurante que he visitado. La comida era deliciosa y el servicio fue excelente. Sin duda volveré pronto.",
-   female: true,
+      "La mejor startup que he visitado. El diseño del logotipo era delicioso y el servicio fue excelente. Sin duda volveré pronto.",
+    female: true,
   },
 ];
